25830987d0df8c912c084faf8e0bf8766c671230
Glitch (bimprod) <none>
2022-09-18 17:39:12 +0000

    Import of BIM-Productions/sound-cloud-bim-prod-storna

---
+++ assets/theme/js/js-kody.js
@@ -0,0 +1,11 @@
+
+
+// JavsScript
+var kontener = document.getElementById("kontener")
+
+<!-- HTML -->
+<input type="email" id="email" />
+
+// JavaScript
+var email = document.getElementById("email").value;
+// Tutaj atrybut '.value' daje nam możliwość odczytania wartości z pobranego elementu.
